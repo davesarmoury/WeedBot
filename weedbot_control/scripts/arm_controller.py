bcad880b50a7f714c97d4707dcb22058bd701507
Dave Niewinski <davesarmoury@gmail.com>
2021-08-17 22:44:13 -0400

    Consolidated localization into the controller node.  Added named and weed list services to the arm controller as poorly as possible.  Untested

--- weedbot_control/scripts/arm_controller.py
+++ weedbot_control/scripts/arm_controller.py
@@ -9,6 +9,11 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
+from weedbot_msgs.srv import NamedArmCommand, NamedArmCommandResponse
+from weedbot_msgs.srv import PoseListArmCommand, PoseListArmCommandResponse
+
+WEED_POUNCE_HEIGHT = 0.25
+WEED_MURDER_HEIGHT = 0.1
 
 J_POSES = {}
 J_POSES["CAMERA"] = [[0.0, 0.785, 0.0, 1.5707, 0.0, 0.785, -1.5707]]
@@ -18,6 +23,55 @@ J_POSES["FLAME ON"] = [
 [-0.0367042187756903, 1.457636082074394, 0.08504282295804119, 1.6726252205512753, -0.036259470873647004, 1.5270838664526085, -1.5579966419637579],
 [0.28236947761135334, 1.4930424092928343, 0.2976072864204597, 1.4716469725693009, 0.2701074517147879, 1.6016244137833813, -1.3077852006997617]
 ]
+
+```Yes, services for this kind of process are not even close to best practice.  Lazy Dave```
+def named_callback(req):
+    if req.data in J_POSES:
+        for i in J_POSES[req.data]:
+            group.go(i, wait=True)
+            group.stop()
+
+        return NamedArmCommandResponse("Success")
+    else:
+        return NamedArmCommandResponse("No pose named: " + str(req.data))
+
+```Yes, services for this kind of process are not even close to best practice.  Lazy Dave```
+def weeds_callback(req):
+    for pose_goal in req.data.poses:
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = 1.0
+        pose_goal.position.x = pose.position.x
+        pose_goal.position.y = pose.position.y
+        pose_goal.position.z = pose.position.z + WEED_POUNCE_HEIGHT
+        group.set_pose_target(pose_goal)
+
+        plan = group.go(wait=True)
+        group.stop()
+        group.clear_pose_targets()
+
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = 1.0
+        pose_goal.position.x = pose.position.x
+        pose_goal.position.y = pose.position.y
+        pose_goal.position.z = pose.position.z + WEED_MURDER_HEIGHT
+        group.set_pose_target(pose_goal)
+
+        plan = group.go(wait=True)
+        group.stop()
+        group.clear_pose_targets()
+
+        rospy.sleep(5.0)
+
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = 1.0
+        pose_goal.position.x = pose.position.x
+        pose_goal.position.y = pose.position.y
+        pose_goal.position.z = pose.position.z + WEED_POUNCE_HEIGHT
+        group.set_pose_target(pose_goal)
+
+        plan = group.go(wait=True)
+        group.stop()
+        group.clear_pose_targets()
 
 def main()
     global group
@@ -49,13 +103,12 @@ def main()
     print robot.get_current_state()
     print ""
 
+    rospy.Service('/weeds/move_arm/named', NamedArmCommand, named_callback)
+
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
         print str(robot.get_current_state())
         rate.sleep()
-
-    #group.go([0.08559498779817709, 1.1641755704977832, -3.1233108516547263, -1.5473972608252424, -0.040984983911882544, -0.4232918784049007, 1.702083244316969], wait=True)
-    #group.stop()
 
 if __name__ == '__main__':
     try:
--- weedbot_control/scripts/weed_coordinator.py
+++ weedbot_control/scripts/weed_coordinator.py
@@ -3,9 +3,39 @@
 import rospy
 from std_msgs.msg import String
 from weedbot_msgs.msg import Detection
-from weedbot_msgs.srv import Localize, PoseListArmCommand, NamedArmCommand
+from weedbot_msgs.srv import PoseListArmCommand, NamedArmCommand
+from geometry_msgs.msg import Pose, PoseArray
+import math
+
 STATUS = "Starting"
 HANDLED = True
+
+GROUND_TO_ARM = 15.0 * 25.4 / 1000.0
+ARM_TO_CAM = 0.1
+GROUND_TO_CAM = GROUND_TO_ARM + ARM_TO_CAM
+ARM_REACH = 0.5
+VERTICAL_DEGREES = 65/1920*1080
+IMG_W = 416
+IMG_H = 416
+DEGREES_PER_PIXEL = VERTICAL_DEGREES / IMG_H
+RAD_PER_PIXEL = DEGREES_PER_PIXEL * 0.0174533
+
+def localize_weeds(objects):
+    pose_array_msg = PoseArray()
+    for weed in objects:
+        pose_msg = Pose()
+        avg_x = ( weed.x1 + weed.x2 ) / 2.0
+        avg_y = ( weed.y1 + weed.y2 ) / 2.0
+        angle_horizontal = ( avg_x - IMG_W / 2.0 ) * RAD_PER_PIXEL
+        angle_vertical = ( avg_y - IMG_H / 2.0 ) * RAD_PER_PIXEL * (-1)
+        X = math.tan(angle_vertical) * GROUND_TO_CAM
+        Y = math.tan(angle_horizontal) * GROUND_TO_CAM
+        pose_msg.position.x = X
+        pose_msg.position.Y = Y
+        pose_msg.position.Y = GROUND_TO_CAM * (-1)
+        pose_array_msg.poses(pose_msg)
+
+    return pose_array_msg
 
 def move_callback(msg):
     global STATUS, HANDLED
@@ -30,8 +60,6 @@ def Control():
 
             weeds_detected = rospy.wait_for_message("/weeds/detection/raw", Detection)
             if len(weeds_detected.objects) > 0:
-                rospy.wait_for_service('/weeds/localize_weeds')
-                localizer = rospy.ServiceProxy('/weeds/localize_weeds', Localize)
                 weeds_localized = localize_weeds(weeds_detected)
                 named_arm_mover("FLAME ON")
 
